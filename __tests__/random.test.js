04c39ce10d89eeca98f4d78c52a664554fa279a6
Travis Swift <travis.arthur.swift@gmail.com>
2022-04-14 21:31:10 -0500

    first faliled and passed test

---
+++ __tests__/random.test.js
@@ -0,0 +1,9 @@
+const checkIfEqual = require('../lib/random.js');
+
+// test('checks if 10 is equal to 10', () => {
+//   expect(checkIfEqual(10, 10)).toBe(true);
+// });
+
+test('checks if 10 is equal to 10', () => {
+    expect(checkIfEqual(20, 20)).toBe(true);
+});
